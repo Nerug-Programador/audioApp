61c8167c58d7364c765c26a166c8eaafe8cd3bad
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2024-10-28 21:05:45 -0500

    Modelando nuestra aplicación de audio.

---
+++ src/com/nerugprogramador/audioaApp/modelos/Cancion.java
@@ -0,0 +1,31 @@
+package com.nerugprogramador.audioaApp.modelos;
+
+public class Cancion extends Audio {
+    private String album;
+    private String cantante;
+    private String genro;
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getCantante() {
+        return cantante;
+    }
+
+    public void setCantante(String cantante) {
+        this.cantante = cantante;
+    }
+
+    public String getGenro() {
+        return genro;
+    }
+
+    public void setGenro(String genro) {
+        this.genro = genro;
+    }
+}
